3354252b13249f34f5447e9a2fd9ba83c0a7ed69
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-22 06:12:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
-def reverse_each_word(sentence1)
-  reversed = sentence1.split(" ").collect do |word|
-  word.reverse
-  end
-  reversed.join(" ")
-end
+#def reverse_each_word(sentence1)
+#  reversed = sentence1.split(" ").collect do |word|
+#  word.reverse
+#  end
+#  reversed.join(" ")
+#end
 
-# def reverse_each_word(sentence1)
-#   split_sentence1 = sentence1.split(" ")
-#   reversed_sentence1 = split_sentence1.each do |word|
-#   word.reverse
-#   end
-#   return reversed_sentence1.join(" ")
-# end
+ def reverse_each_word(sentence1)
+   split_sentence1 = sentence1.split(" ")
+   reversed_sentence1 = split_sentence1.each do |word|
+   word.reverse
+   end
+   puts reversed_sentence1.join(" ")
+ end
